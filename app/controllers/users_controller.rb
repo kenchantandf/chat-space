83b120c70a86f25425650c1bedceb912f3c116f2
Kento Terao <kenchan1020.com@gmail.com>
2017-03-04 18:51:48 +0900

    For flash messages
    フラッシュメッセージに必要な表記を実装。また、編集ができた時とでなかった時のフラッシュメッセージを実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_with_password(user_params)
-      redirect_to root_path
+      redirect_to root_path, notice: 'アカウント情報を変更しました'
     else
+      flash.alert = 'アカウント情報を変更できませんでした'
       render action: :edit
     end
   end
