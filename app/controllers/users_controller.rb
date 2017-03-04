bf4d5f87e3844882a50406623d80c253f16ac4d6
Kento Terao <kenchan1020.com@gmail.com>
2017-03-04 18:13:24 +0900

    Creating update action
    updateの実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_with_password(user_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
