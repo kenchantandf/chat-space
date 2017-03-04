1338ec7fdf66390ae4fef4e7273e6e4cca31703e
Kento Terao <kenchan1020.com@gmail.com>
2017-03-04 19:32:50 +0900

    changed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,13 @@ class UsersController < ApplicationController
     if @user.update_with_password(user_params)
       redirect_to root_path, notice: 'アカウント情報を変更しました'
     else
-      flash.alert = 'アカウント情報を変更できませんでした'
-      render action: :edit
+      flash.now[:alert] = 'アカウント情報を変更できませんでした'
+      render :edit
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
