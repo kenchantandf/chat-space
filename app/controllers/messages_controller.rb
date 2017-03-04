95a184bcd879b4485dd2710ea558a07118c18a5d
Kento Terao <kenchan1020.com@gmail.com>
2017-03-04 16:02:07 +0900

    Redirect to index
    サインインしていない場合、indexアクションが動くように記述を加えた。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,12 @@
 class MessagesController < ApplicationController
+
+before_action :move_to_index, except: :index
+
+  def index
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
