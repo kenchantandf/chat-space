9eeaee08701d3c4740d3b119fb4bb993d02df9a0
Kento Terao <kenchan1020.com@gmail.com>
2017-03-04 16:39:11 +0900

    Use authenticate_user!
    authenticate_userを利用し、ログイン画面へのリダイレクトを可能にしました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,8 @@
 class MessagesController < ApplicationController
 
-before_action :move_to_index, except: :index
+before_action :authenticate_user!
 
   def index
   end
 
-  private
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
 end
