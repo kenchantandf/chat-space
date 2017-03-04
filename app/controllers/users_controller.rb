348cafcd6b8c76cea888632e31910994bde9aed0
Kento Terao <kenchan1020.com@gmail.com>
2017-03-04 17:08:43 +0900

    Update available
    編集した情報が更新されるように記述しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_with_password(update_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
   end
 
+  private
+  def update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
 end
