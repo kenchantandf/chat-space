6b6ed3edd61a79b753dcc00c3f7c9c3ec3f666bc
Kento Terao <kenchan1020.com@gmail.com>
2017-03-05 12:59:59 +0900

    changing root to the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'messages#index'
   resources :users, only: [:edit, :update]
   resources :messages, only: [:index]
   resources :groups, only: [:new, :create, :edit, :update]
-  root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
