d52c0b61165c1d20c4efa845c3bb8175bf3dc804
Kento Terao <kenchan1020.com@gmail.com>
2017-03-08 21:19:59 +0900

    Edited groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,33 @@
 class GroupsController < ApplicationController
+
+  before_action :set_on_group,except: [:new,:create]
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+      if @group.save
+         redirect_to root_path(@group),notice: 'グループの作成が完了しました。'
+      else
+        flash.now[:alert] = "グループの作成に失敗しました。"
+        render :new
+      end
+  end
+
   def edit
   end
+
+
+private
+
+  def set_on_group
+    @group = Group.find(params[:id])
+  end
+
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
 end
