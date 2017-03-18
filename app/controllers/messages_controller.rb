bdf8f4b30bb97fc78d88b4478a98c28094abab95
Kento Terao <kenchan1020.com@gmail.com>
2017-03-18 23:16:54 +0900

    edited messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,14 @@
 class MessagesController < ApplicationController
-  before_action :set_on_group, only: [:index]
+  before_action :set_on_group, :set_on_groups
 
   def index
   end
 
   private
+
+  def set_on_groups
+    @groups = current_user.groups
+  end
 
   def set_on_group
     # @group = Group.find(params[:group_id])
