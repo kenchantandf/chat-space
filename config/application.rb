22a04bb94df6cf734da0a4bcf87cd1ab8703a464
Kento Terao <kenchan1020.com@gmail.com>
2017-04-07 10:11:50 +0900

    Changed time zone
    時間表記をUTCからJSTに変更。

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,7 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja #コメントアウトしている場合は外すだけで良い。
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
