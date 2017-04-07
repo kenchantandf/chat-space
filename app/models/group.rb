deb13c50f0548ea363fe125e96326c9b4b142c1b
Kento Terao <kenchan1020.com@gmail.com>
2017-04-07 14:36:52 +0900

    Method
    メソッド化

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,8 @@ has_many :users, through: :group_users
 accepts_nested_attributes_for :users
 
 validates :name, presence: true
+
+def last_message
+  self.messages.present? ? self.messages.last.text : 'まだメッセージはありません。'
+end
 end
