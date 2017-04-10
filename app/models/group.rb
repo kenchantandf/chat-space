b8262b2bb0bbb432a010519870f8ba750af6223e
Kento Terao <kenchan1020.com@gmail.com>
2017-04-10 18:02:46 +0900

    Deleted self
    レシーバーの省略。

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,6 +8,6 @@ accepts_nested_attributes_for :users
 validates :name, presence: true
 
 def last_message
-  self.messages.present? ? self.messages.last.text : 'まだメッセージはありません。'
+  messages.present? ? messages.last.text : 'まだメッセージはありません。'
 end
 end
