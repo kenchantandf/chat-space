bff8a44a73eac65e8003aafa1a81d93e9cb20673
Kento Terao <kenchan1020.com@gmail.com>
2017-04-24 18:09:02 +0900

    Edited message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,37 @@
+$(function() {
+  function buildHTML(message) {
+    var html = $(`<li class="chat-message">
+                    <div class="chat-message__header">
+                      <p class="chat-message__user"> ${message.name} </p>
+                      <p class="chat-message__time"> ${message.time} </p>
+                    </div>
+                    <p class="chat-message__body"> ${message.text} </p>
+                  </li>`);
+    return html;
+  }
+
+  $('#new-message').on("submit", function(e) {
+    e.preventDefault();
+    var textField = $('.chat-footer__body__textarea');
+    var message = textField.val();
+    $.ajax({
+      type: 'POST',
+      url: './messages',
+      data: {
+        message: {
+          text: message
+        }
+      },
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.chat-messages').append(html);
+      $('#new-message')[0].reset();
+    })
+    .fail(function() {
+      alert('ALERT');
+    });
+    return false;
+  });
+});
