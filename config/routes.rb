588d27716aa17b3c401b870b805838f81b5484fa
Kento Terao <kenchan1020.com@gmail.com>
2017-05-01 18:03:20 +0900

    Edited routes.rb
    インクリメンタルサーチを実装するにあたって、routes.rbを変更。

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, except: [:show, :destroy] do
-    resources :messages, only: [:index, :new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i(edit update) do
+    collection do
+      get 'search'
+    end
   end
+  resources :groups, except: %i(show destroy) do
+    resources :messages, only: %i(index create)
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
