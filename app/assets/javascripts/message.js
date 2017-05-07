a959242e6c3c58e1911d7ea4ae77e6ac537fe308
Kento Terao <kenchan1020.com@gmail.com>
2017-05-07 18:07:33 +0900

    Changed class name to double quotation

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,11 @@
 $(function() {
   function buildHTML(message) {
-    var html = $(`<li class='chat-message'>
-                    <div class='chat-message__header'>
-                      <p class='chat-message__user'> ${message.name} </p>
-                      <p class='chat-message__time'> ${message.time} </p>
+    var html = $(`<li class="chat-message">
+                    <div class="chat-message__header">
+                      <p class="chat-message__user"> ${message.name} </p>
+                      <p class="chat-message__time"> ${message.time} </p>
                     </div>
-                    <p class='chat-message__body'> ${message.text} </p>
+                    <p class="chat-message__body"> ${message.text} </p>
                   </li>`);
     return html;
   }
