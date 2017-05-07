35f9a503f935e87925683fdd00d42d84cf302f4f
Kento Terao <kenchan1020.com@gmail.com>
2017-05-07 20:00:07 +0900

    Added config to application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module ChatSpace
     config.i18n.default_locale = :ja #コメントアウトしている場合は外すだけで良い。
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end
