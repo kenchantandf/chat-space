3f2084d6587ab981debcf42b5038b2fde07220ce
Kento Terao <kenchan1020.com@gmail.com>
2017-05-07 23:10:32 +0900

    Deleted unnecessary coding

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module ChatSpace
     config.i18n.default_locale = :ja #コメントアウトしている場合は外すだけで良い。
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
 end
