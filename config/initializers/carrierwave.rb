c0ddd762a5802622c5d989d983f687cbd6815609
Kento Terao <kenchan1020.com@gmail.com>
2017-05-08 15:43:11 +0900

    edited url for carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,18 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
+    region: 'us-west-1'
   }
-
 
   case Rails.env
   when 'development'
     config.storage :fog
     config.fog_directory  = 'chat-space-expert'
-    config.asset_host = 'https://s3.amazonaws.com/chat-space-expert'
+    config.asset_host = 'https://s3-us-west-1.amazonaws.com/chat-space-expert'
   when 'production'
     config.storage :fog
     config.fog_directory  = 'chat-space-expert'
-    config.asset_host = 'https://s3.amazonaws.com/chat-space-expert'
+    config.asset_host = 'https://s3-us-west-1.amazonaws.com/chat-space-expert'
   when 'test'
     config.storage :file
   end
