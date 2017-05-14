f72f32f5253576493e62581e08ffdc25b69b934f
Kento Terao <kenchan1020.com@gmail.com>
2017-05-14 17:42:08 +0900

    Refactoring

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,71 +1,81 @@
 $(document).on('turbolinks:load', function() {
-  $('#new-message').on("submit", function(e) {
+  $('#new-message').on('submit', function(e) {
     e.preventDefault();
-    AjaxSend();
-    return false;
+
+    sendMessageAjax = sendMessageAjax.bind(this);
+    
+    sendMessageAjax();
   });
-  pageReload();
+  reloadPage();
 });
 
 function buildHTML(message) {
-  var image = (message.image) ? `<img src = ${ message.image }>` : '';
-  var html = `<li class="chat-message">
-                <div class="chat-message__header">
-                  <p class="chat-message__user">${ message.name }</p>
-                  <p class="chat-message__time">${ message.time }</p>
-                </div>
-                <p class="chat-message__body">${ message.text }</p>
-                <p>${ image }</p>
-              </li>`;
-  return html;
+  var image = message.image ? `<img src = ${message.image}>` : '';
+
+  return `
+    <li class="chat-message">
+      <div class="chat-message__header">
+        <p class="chat-message__user">${ message.name }</p>
+        <p class="chat-message__time">${ message.time }</p>
+      </div>
+      <p class="chat-message__body">${ message.text }</p>
+      ${ image }
+    </li>  
+  `;
 }
 
-function scrollBottom() {
+function scrollToBottom() {
   $('.chat-context').animate({
     scrollTop: $('.chat-messages').height()
   });
 }
 
-function AjaxSend(){
-    $.ajax({
-      type: 'POST',
-      url: './messages',
-      data: new FormData($("#new-message").get(0)),
-      processData: false,
-      contentType: false,
-      dataType: 'json'
-    })
-    .done(function(data) {
-      var html = buildHTML(data);
-          insertHTML = $("#new-message").get(0);
-      $('.chat-messages').append(html);
-      insertHTML.reset();
-      scrollBottom();
-    })
-    .fail(function() {
-      alert('メッセージを入力してください。');
-    });
-  }
+function sendMessageAjax() {
+  var url = $(this).attr('action'),
+      formData = new FormData(this);
 
-function pageReload(){
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: formData,
+    processData: false,
+    contentType: false,
+    dataType: 'json',
+    context: this,
+  })
+  .done(function(data) {
+    var html = buildHTML(data);
+    $('.chat-messages').append(html);
+    this.reset();
+
+    scrollToBottom();
+  })
+  .fail(function() {
+    alert('メッセージを入力してください。');
+  });
+}
+
+function reloadPage() {
   setInterval(function() {
     $.ajax({
       type: 'GET',
       url: './messages',
-      dataType: 'json'
+      dataType: 'json',
     })
-    .done(function (data){
-      var reloadMessage = $('.chat-message').length;
-          newMessage = data.messages.length;
-          buildMessage ='';
-        for(var i = reloadMessage; i < newMessage; i++){
-           buildMessage += buildHTML(data.messages[i]);
-        };
-     $('.chat-messages').append(buildMessage);
-      scrollBottom();
+    .done(function(data) {
+      var currentMessagesNum = $('.chat-message').length,
+          newMessgaesNum = data.messages.length,
+          newMessages = data.messages.slice(currentMessagesNum, newMessgaesNum),
+          newMessagesHTML = '';
+
+      $.each(newMessages, function(i, message) {
+        $('.chat-messages').append(buildHTML(message));
+      });
+
+      scrollToBottom();
     })
-    .fail(function(){
-      alert("エラーが発生しました。");
-    })
+    .fail(function() {
+      alert('エラーが発生しました。');
+    });
   }, 5000);
 }
